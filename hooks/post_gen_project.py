76405a99442fba896062ba3948118e1724b3dde8
Fer Aguirre <kay_op@protonmail.com>
2022-03-23 21:22:28 -0600

    Update hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,7 +11,8 @@ os.system("pip install pipenv")
 print(f"{MESSAGE_COLOR}Creating virtual environment...{RESET_ALL}")
 os.system("pipenv install")
 os.system("pipenv sync")
-os.system("pipenv install '-e .' --dev")
+os.system("python3 setup.py install")
+os.system("pipenv install -e .")
 
 # Initialize git
 print(f"{MESSAGE_COLOR}Initializing a git repository...{RESET_ALL}")
