977dcfddfe521dce623242cf80f267f526cce5b2
Fer Aguirre <kay_op@protonmail.com>
2022-04-28 21:17:49 -0500

    Add command to change master branch to main

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,13 +10,14 @@ os.system("pip install pipenv")
 # Install virtual environment and synchronize packages
 print(f"{MESSAGE_COLOR}Creating virtual environment...{RESET_ALL}")
 os.system("pipenv install")
+os.system("pipenv shell")
 os.system("pipenv sync")
 os.system("python3 setup.py install")
 os.system("pipenv install -e .")
 
 # Initialize git
 print(f"{MESSAGE_COLOR}Initializing a git repository...{RESET_ALL}")
-os.system("git init && git add . && git commit -m 'Initial commit'")
+os.system("git init && git add . && git commit -m 'Initial commit' && git branch -M main")
 
 # Final message
 print(f"{MESSAGE_COLOR}Your template for {{cookiecutter.project_name}} is ready!{RESET_ALL}")
