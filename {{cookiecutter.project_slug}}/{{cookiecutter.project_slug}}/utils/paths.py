491cbf24ed9e6ccb60e6fb7fa087facb2cbac36c
Fer Aguirre <kay_op@protonmail.com>
2022-05-03 23:31:21 -0500

    Modify comment for function on paths.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/paths.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/utils/paths.py
@@ -9,17 +9,15 @@ from typing import (
 def make_dir_function(
     dir_name: Union[str, Iterable[str]]
 ) -> Callable[..., Path]:
-    """Generate a function that converts a string or iterable of strings into
+    """
+    Generate a function that converts a string or iterable of strings into
     a path relative to the project directory.
-    Args:
-        dirname: Name of the subdirectories to extend the path of the main
-            project.
-            If an iterable of strings is passed as an argument, then it is
-            collapsed to a single steing with anchors dependent on the
-            operating system.
-    Returns:
-        A function that returns the path relative to a directory that can
-        receive 'n' number of arguments for expansion.
+
+    @param dirname: Name of the subdirectories to extend the path of the main
+    project.
+
+    @returns A function that returns the path relative to a directory that can
+    receive 'n' number of arguments for expansion.
     """
 
     def dir_path(*args) -> Path:
