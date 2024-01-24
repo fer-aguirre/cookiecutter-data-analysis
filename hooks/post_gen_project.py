360e0029f46f0de3a4f0a1545a08df8c006f331b
Fer Aguirre <77854667+fer-aguirre@users.noreply.github.com>
2024-01-23 20:27:19 -0600

    Set up python 3.7 version for pipenv

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,10 +9,10 @@ os.system("pip install pipenv")
 
 # Install virtual environment and synchronize packages
 print(f"{MESSAGE_COLOR}Creating virtual environment...{RESET_ALL}")
-os.system("pipenv install")
+os.system("pipenv install --python 3.7")
 os.system("pipenv sync")
 os.system("pipenv update setuptools")
-os.system("python3 setup.py install")
+os.system("python setup.py install")
 os.system("pipenv install -e .")
 
 # Initialize git
